136db86631df03a899ba579c5f8f59f07a612129
CroquetteTheThe <dadou72leboss@yahoo.fr>
2019-01-26 06:24:45 +0100

    Some code for collisions

--- Player.gd
+++ Player.gd
@@ -6,11 +6,15 @@ extends Area2D
 
 export (int) var speed
 var screensize
+var can_move_right
+signal hit
 
 func _ready():
 	screensize = get_viewport_rect().size
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
+	can_move_right = true
+	
 	pass
 
 #func _process(delta):
@@ -21,21 +25,31 @@ func _ready():
 func _process(delta):
 	var velocity = Vector2()
 	velocity.y += 0.5
-	if Input.is_action_pressed("ui_right"):
+	if Input.is_key_pressed(KEY_D) and can_move_right:
 		velocity.x += 1
-	if Input.is_action_pressed("ui_left"):
+		$AnimatedSprite.flip_h = false
+	if Input.is_key_pressed(KEY_Q):
 		velocity.x -= 1
-	if Input.is_action_pressed("ui_down"):
-        velocity.y += 1
-	if Input.is_action_pressed("ui_up"):
-        velocity.y -= 1
+		$AnimatedSprite.flip_h = true
+	if Input.is_key_pressed(KEY_SPACE):
+		$AnimatedSprite.animation = "prepare_attack"
+	elif $AnimatedSprite.animation == "prepare_attack":
+		$AnimatedSprite.animation = "attack"
+	elif $AnimatedSprite.animation == "attack":
+		$AnimatedSprite.animation = "normal"
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
-	if velocity.x > 0:
+	if velocity.x != 0:
 		$AnimatedSprite.play()
 	else:
 		$AnimatedSprite.stop()
 	position += velocity * delta
 	position.x = clamp(position.x, 0, screensize.x)
 	position.y = clamp(position.y, 10, screensize.y/2)
-		
+	
+
+func _on_Player_area_entered(area):
+	can_move_right = false
+
+func _on_Player_area_exited(area):
+	can_move_right = true
